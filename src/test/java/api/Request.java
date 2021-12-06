67807cf7e2ecae76ab76072be72434cd9eb9646d
Jardeson Santos <JarDeVSon@github.com>
2021-12-06 15:28:07 -0300

    Lição - Criando interfaces verbos

---
+++ src/test/java/api/Request.java
@@ -0,0 +1,29 @@
+package api;
+
+public class Request implements Verbos{
+
+    @Override
+    public void POST() {
+
+    }
+
+    @Override
+    public void GET() {
+
+    }
+
+    @Override
+    public void PUT() {
+
+    }
+
+    @Override
+    public void PATCH() {
+
+    }
+
+    @Override
+    public void DELETE() {
+
+    }
+}
---
+++ src/test/java/api/Verbos.java
@@ -0,0 +1,9 @@
+package api;
+
+public interface Verbos {
+    void POST();
+    void GET();
+    void PUT();
+    void PATCH();
+    void DELETE();
+}
